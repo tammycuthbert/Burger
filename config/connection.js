a6495fb692f2c539885e154f51c7a72c7b460f25
Tammy Cuthbert <tammycuthbert427@gmail.com>
2020-08-08 23:21:30 -0500

    updates

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db',
+    })
+}
+
+connection.connect(function(err) {
+
+    if (err) {
+        console.error(`Error connection: ${err.stack}`);
+        return;
+    }
+
+    console.log(`Connected as id ${connection.threadId}`);
+})
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('McChicken', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Whopper', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Boca Burger', false);
